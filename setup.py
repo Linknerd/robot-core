e142dc4995937c6fa72145035c54f8e13154ac89
Alan Zheng <zheng.alan.work@gmail.com>
2026-03-22 16:44:07 -0400

    Fixing file structure

---
+++ setup.py
@@ -0,0 +1,24 @@
+package_name = 'robot_core'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='your_name',
+    maintainer_email='your_email@example.com',
+    description='Robot core package',
+    license='MIT',
+    entry_points={
+        'console_scripts': [
+            'serial_bridge = robot_core.serial_bridge:main',
+            'robot_logic = robot_core.robot_logic:main',
+        ],
+    },
+)
